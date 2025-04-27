7d0d8a506d97b1dd7771fc20210524bde5de52e6
Ankit-Raskar <ankitraskar01@gmail.com>
2025-04-27 23:33:39 +0530

    add new js and html

--- script.js
+++ script.js
@@ -6,3 +6,20 @@ buyButtons.forEach((button) => {
         alert(`Thank you for buying ${gameTitle}! 🎮`);
     });
 });
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    let topBtn = document.getElementById("topBtn");
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        topBtn.style.display = "block";
+    } else {
+        topBtn.style.display = "none";
+    }
+}
+
+
+function scrollToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
